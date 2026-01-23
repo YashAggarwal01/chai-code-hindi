b492d01737f2fba203ee31fb776e9005236d176c
YashAggarwal01 <mr.yashgamerzyt@gmail.com>
2026-01-23 10:26:22 +0000

    ll

---
+++ 04controlflow/one.js
@@ -0,0 +1,30 @@
+//regular if else consition as ordinary Java program 
+
+if(true){
+    //condition
+}
+else{
+    //condition
+}
+
+//Short hand notation
+const balance = 1100
+
+
+if(balance>500) console.log("Test implicit if-else");
+
+
+//if else ladder is as usual in java
+
+
+if(true){
+    //First condition
+}
+else if(true){
+    //second condition
+}
+else{
+    //Third condition
+}
+
+
---
+++ 04controlflow/switch.js
@@ -0,0 +1,15 @@
+//as usual switch case in ordinary java
+
+//syntax
+// switch(key){
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+
+const month =3 
+switch(month){
+    case
+}
